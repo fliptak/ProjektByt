03259f39a77985be48c4b307c11c020a3f95c24c
Saszqa <66468083+Saszqa@users.noreply.github.com>
2022-02-03 02:52:55 +0100

    bez logowania ale button jest

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import HomePage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/coins/:id" element= {<CoinPage />} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ src/components/SelectButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid gold",
+      borderRadius: 5,
+      paddingTop: 9,
+      paddingBottom: 9,
+      paddingLeft: 10,
+      paddingRight: 10,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "gold" : "",
+      color: selected ? "black" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "gold",
+        color: "black",
+      },
+      marginLeft: 5,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
